350b23422a15d807f5055fb28474e8f5149e8cda
Mejor <amujobabatunde7@gmail.com>
2022-11-20 20:10:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UserPage from './pages/userPage/usePage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserPage />
+    </div>
+  );
+}
+
+export default App;
